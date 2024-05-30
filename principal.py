66e0fdd04033f0dd82d1744c26ac37d09e7a29e5
André Ferreira <andreffilho45@gmail.com>
2024-05-30 17:40:18 -0300

    Alteração do layout da pagina principal e funcionalidade arrasta e solta
    - Adicionada coluna "Tarefas Concluídas" para exibir as tarefas concluídas.
    - Implementada funcionalidade de arrastar e soltar para mover tarefas entre as colunas "Lista de Tarefas" e "Tarefas Concluídas".

--- principal.py
+++ principal.py
@@ -12,8 +12,15 @@ class Principal(ft.SafeArea):
         self.item: ft.TextField = ft.TextField(**item_style_sheet)
         self.add: ft.IconButton = ft.IconButton(**add_style_sheet, on_click=self.add_board)
 
-        self.area_tarefas: ft.Column = ft.Column(expand=True, spacing=18)
+        self.area_tarefas: ft.DragTarget = ft.DragTarget(
+            content=ft.Column(spacing=18, height=680, scroll="Auto", controls=[ft.Container()]),
+            group="tarefa"
+        )
         self.counter: ft.Text = ft.Text("0 itens", italic=True)
+        self.area_concluida: ft.DragTarget = ft.DragTarget(
+            content=ft.Column(spacing=18, height=680, scroll="Auto", controls=[ft.Container()]),
+            group="tarefa"
+        )
 
     def tela_tarefa(self):
         self.main: ft.Container = ft.Container(
@@ -30,16 +37,67 @@ class Principal(ft.SafeArea):
                             self.toggle
                         ],
                     ),
-                    ft.Divider(height=20), 
-                    ft.Divider(height=10, color="transparent"),
-                    ft.Row(
+                    ft.Divider(height=20),
+                    ft.ResponsiveRow(
                         alignment="spaceBetween",
+                        columns=100,
                         controls=[
-                            ft.Text("Lista de tarefas"),
-                            self.counter, 
-                        ]
-                    ),
-                    self.area_tarefas,
+                            ft.Column(
+                                col={
+                                    "xs": 51
+                                },
+                                controls=[
+                                    ft.Container(
+                                        content=ft.Column(
+                                            scroll=ft.ScrollMode.AUTO,
+                                            controls=[
+                                                ft.Row(
+                                                    alignment="spaceBetween",
+                                                    col=2,
+                                                    controls=[
+                                                        ft.Text('Lista de Tarefas'),
+                                                        self.counter
+                                                    ]
+                                                ),
+                                                self.area_tarefas
+                                            ],
+                                        ),
+                                        bgcolor=ft.colors.with_opacity(0.5,"grey50"),
+                                        border_radius=5,
+                                        border=ft.border.all(1,"grey50"),
+                                        padding=10,
+                                    )
+                                ],
+                            ),
+                            ft.Column(
+                                col={
+                                    "xs": 49
+                                },
+                                controls=[
+                                    ft.Container(
+                                        content=ft.Column(
+                                            controls=[
+                                                ft.Row(
+                                                    controls=[
+                                                        ft.Text("Tarefas Concluídas"),
+                                                        ft.Container(
+                                                            expand=True
+                                                        )
+                                                    ]
+                                                ),
+                                                self.area_concluida
+                                            ],
+                                        ),
+                                        bgcolor=ft.colors.with_opacity(0.5,"grey50"),
+                                        border_radius=5,
+                                        border=ft.border.all(1,"grey50"),
+                                        padding=10,
+                                    )
+                                ],
+                            ),
+                        ],
+                        height=500
+                    )
                 ],
             ),
             expand=True,
@@ -51,7 +109,7 @@ class Principal(ft.SafeArea):
                     ft.colors.YELLOW_200
                 ]
             ),
-            padding=ft.padding.all(20)
+            padding=ft.padding.all(20),
         )
 
         self.page.theme = ft.Theme(
@@ -61,23 +119,22 @@ class Principal(ft.SafeArea):
                 )
             )
 
-        self.content = self.main
-        return self.content
+        return self.main
 
     def item_size(self) -> None:
-        if len(self.area_tarefas.controls[:]) == 1:
-            self.counter.value = f"{len(self.area_tarefas.controls[:])} item"
+        if len(self.area_tarefas.content.controls[:]) == 2:
+            self.counter.value = f"{len(self.area_tarefas.content.controls[:]) - 1} item"
         else:
-            self.counter.value = f"{len(self.area_tarefas.controls[:])} itens"
+            self.counter.value = f"{len(self.area_tarefas.content.controls[:]) - 1} itens"
 
         self.counter.update()
 
     def add_item(self, dialog_text: str) -> None:
         if dialog_text != "":
             if self.page.theme_mode == ft.ThemeMode.DARK:
-                self.area_tarefas.controls.append(Tarefa(self, dialog_text, "dark"))
+                self.area_tarefas.content.controls.append(Tarefa(self, dialog_text, "dark"))
             else:
-                self.area_tarefas.controls.append(Tarefa(self, dialog_text, "light"))
+                self.area_tarefas.content.controls.append(Tarefa(self, dialog_text, "light"))
 
             self.area_tarefas.update()
             self.item_size()
@@ -112,7 +169,7 @@ class Principal(ft.SafeArea):
                 ]
             )
 
-            for item in self.area_tarefas.controls[:]:
+            for item in self.area_tarefas.content.controls[:]:
                 item.border = ft.border.all(1, _light)
 
         else:
@@ -138,7 +195,7 @@ class Principal(ft.SafeArea):
                 ]
             )
 
-            for item in self.area_tarefas.controls[:]:
+            for item in self.area_tarefas.content.controls[:]:
                 item.border = ft.border.all(1, _dark)
 
         self.page.update()
--- tarefa.py
+++ tarefa.py
@@ -2,7 +2,7 @@ from flet import *
 import flet as ft
 from styles import _dark, _light, tarefa_style_sheet
 
-class Tarefa(ft.Container):
+class Tarefa(ft.Draggable):
     def __init__(self, tela_tarefa: object, description: str, theme: str) -> None:
         
         if theme == "dark":
@@ -10,7 +10,7 @@ class Tarefa(ft.Container):
         else:
             tarefa_style_sheet["border"] = ft.border.all(1, _light)
         
-        super().__init__(**tarefa_style_sheet)
+        super().__init__(group="tarefa")
         self.tela_tarefa: object = tela_tarefa
         self.description = description
 
@@ -22,30 +22,93 @@ class Tarefa(ft.Container):
             on_click=lambda e: self.delete_text(e),
         )
 
-        self.content: ft.Row = ft.Row(
-            alignment="spaceBetween",
-            controls=[
-                ft.Row(
+        self.content: ft.Container = ft.Container(
+            content=(
+                    ft.Row(
+                    alignment="spaceBetween",
                     controls=[
-                        self.tick,
-                        self.text
-                    ]
-                ),
-                self.delete,
-            ],
+                        ft.Row(
+                            controls=[
+                                self.tick,
+                                self.text
+                            ]
+                        ),
+                        self.delete,
+                    ],
+                )
+            ),
+            **tarefa_style_sheet,
+            expand=True
         )
+
+        self.content_feedback: ft.Container = ft.Container(
+            content=(
+                    ft.Row(
+                    alignment="spaceBetween",
+                    controls=[
+                        ft.Row(
+                            controls=[
+                                self.tick,
+                                self.text
+                            ]
+                        ),
+                        self.delete,
+                    ],
+                )
+            ),
+            **tarefa_style_sheet,
+            bgcolor=ft.colors.with_opacity(0.5,"white")
+        )
+        self.on_drag_complete = self.drag_complete
+        
 
     def strike(self, e) -> None:
         if e.control.value == True:
             self.text.spans[0].style = ft.TextStyle(
                 decoration=ft.TextDecoration.LINE_THROUGH, decoration_thickness=2
             )
+            self.tela_tarefa.area_tarefas.content.controls.remove(self)
+            self.tela_tarefa.area_concluida.content.controls.append(self)
+            self.tela_tarefa.area_tarefas.update()
+            self.tela_tarefa.area_concluida.update()
+            self.tela_tarefa.item_size()
         else:
             self.text.spans[0].style = ft.TextStyle()
+            self.tela_tarefa.area_concluida.content.controls.remove(self)
+            self.tela_tarefa.area_tarefas.content.controls.append(self)
+            self.tela_tarefa.area_concluida.update()
+            self.tela_tarefa.area_tarefas.update()
+            self.tela_tarefa.item_size()
 
         self.text.update()
 
     def delete_text(self, e) -> None:
-        self.tela_tarefa.area_tarefas.controls.remove(self)
-        self.tela_tarefa.area_tarefas.update()
-        self.tela_tarefa.item_size()
+        if self in self.tela_tarefa.area_tarefas.content.controls:
+            self.tela_tarefa.area_tarefas.content.controls.remove(self)
+            self.tela_tarefa.area_tarefas.update()
+            self.tela_tarefa.item_size()
+        else:
+            self.tela_tarefa.area_concluida.content.controls.remove(self)
+            self.tela_tarefa.area_concluida.update()
+        
+    def drag_complete(self, e):
+        if e.control.content.content.controls[0].controls[0].value == False:
+            self.text.spans[0].style = ft.TextStyle(
+                decoration=ft.TextDecoration.LINE_THROUGH, decoration_thickness=2
+            )
+            e.control.content.content.controls[0].controls[0].value = True
+            self.tela_tarefa.area_tarefas.content.controls.remove(self)
+            self.tela_tarefa.area_concluida.content.controls.append(self)
+            self.tela_tarefa.area_tarefas.update()
+            self.tela_tarefa.area_concluida.update()
+            self.tela_tarefa.item_size()
+        else:
+            self.text.spans[0].style = ft.TextStyle()
+            e.control.content.content.controls[0].controls[0].value = False
+            self.tela_tarefa.area_concluida.content.controls.remove(self)
+            self.tela_tarefa.area_tarefas.content.controls.append(self)
+            self.tela_tarefa.area_concluida.update()
+            self.tela_tarefa.area_tarefas.update()
+            self.tela_tarefa.item_size()
+
+        self.text.update()
